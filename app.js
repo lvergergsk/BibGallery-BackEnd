acbec4b17d9dc5afdfb917bd71b692c28e5606dd
YOULYU ZHANG <lvergergsk@outlook.com>
2018-04-05 16:00:07 -0400

    wrong username handled, deleted unused files.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 var jwt = require('express-jwt');
 
 const app = express();
-const testQuery = require('./testQuery');
 const registerHandler = require('./handlers/registerHandler');
 const loginHandler = require('./handlers/loginHandler');
 const testHandler=require('./handlers/testHandler');
@@ -18,7 +17,6 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/testDbQuery', testQuery.testQueryHandler);
 app.post('/login', loginHandler);
 app.post('/register', registerHandler);
 app.get('/protected',jwt({secret: process.env.JWT_SECRET}), testHandler);
--- auth/index.js
+++
@@ -1,6 +0,0 @@
-const verifyJWTToken = require("./verifyJWTToken");
-const createJWTToken = require("./createJWTToken");
-const verifyJWT_MW = require("./verifyJWT_MW");
-
-module.exports.verifyJWT_MW=verifyJWT_MW;
-module.exports.createJWTToken=createJWTToken.createJWTToken;
