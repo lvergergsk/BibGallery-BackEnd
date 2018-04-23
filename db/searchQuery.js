55643aec98ee79bd0cbf440501b7ff13eb57df18
kenkangxgwe <kenkangxgwe@gmail.com>
2018-04-23 15:31:11 -0400

    Refine: PUB API.

--- db/searchQuery.js
+++ db/searchQuery.js
@@ -1,12 +1,13 @@
 module.exports = {
     searchType: {
         pub: {
-            begin: `(SELECT RN,PUBLICATION_ID AS ID,TITLE,YEAR,TYPE FROM(SELECT ROWNUM AS RN,PUB.* FROM "YOULYU".PUBLICATION PUB`,
-            end: `)WHERE RN BETWEEN :offset+1 AND :offset+:num)`
+            begin: `WITH TOTTAB AS (SELECT RN,PUBLICATION_ID AS ID,TITLE,YEAR,TYPE FROM(SELECT ROWNUM AS RN,PUB.* FROM(SELECT PUB.* FROM "YOULYU".PUBLICATION PUB`,
+            end: `)PUB)WHERE RN BETWEEN :offset+1 AND :offset+:num)`
         }
     },
     whereClause: ` WHERE(1=1)`,
     title: `AND(REGEXP_LIKE(TITLE,:title,'i'))`,
+    allPubType: ['article', 'inproceeding', 'incollection', 'proceeding', 'book'],
     pubType: {
         begin: `AND((0=1)`,
         article: `OR(TYPE='article')`,
@@ -17,8 +18,20 @@ module.exports = {
         end: `)`
     },
     nameTab: {
-        append: `,"YOULYU".NAME NAME`,
-        person: `AND(REGEXP_LIKE(NAME.PERSON_NAME,:person||,'i'))`
+        append: `,"YOULYU".PUBLISH,"YOULYU".NAME`,
+        person: `AND(REGEXP_LIKE(NAME.PERSON_NAME,:person,'i')AND(PUBLISH.PERSON_ID=NAME.PERSON_ID)AND(PUB.PUBLICATION_ID=PUBLISH.PUBLICATION_ID))`
+    },
+    articleTab: {
+        append: `,"YOULYU".ARTICLE ART`,
+        article: `AND(ART.JOURNAL=:journal)AND(ART.PUBLICATION_ID=PUB.PUBLICATION_ID)`
+    },
+    inproceedingTab: {
+        append: `,"YOULYU".INPROCEEDING INP`,
+        inproceeding: `AND(INP.CROSSREF=:proceedingid)AND(INP.PUBLICATION_ID=PUB.PUBLICATION_ID)`
+    },
+    incollection: {
+        append: `,"YOULYU".INCOLLECTION INC`,
+        incollection: `AND(INP.CROSSREF=:collectionid)AND(INC.PUBLICATION_ID=PUB.PUBLICATION_ID)`
     },
     year: {
         begin: `AND(YEAR>=:yearbegin)`,
@@ -32,6 +45,13 @@ module.exports = {
             ASC: ` ASC`,
             DESC: ` DESC`
         }
+    },
+    refine: {
+        pubbegin: `SELECT * FROM TOTTAB, "YOULYU".`,
+        pubend: ` PUB WHERE TOTTAB.ID = PUB.PUBLICATION_ID`,
+        author: `SELECT RN,MIN(NAM.PERSON_NAME) AS AUTHOR FROM TOTTAB,"YOULYU".PUBLISH PUB,"YOULYU".PERSON PER,"YOULYU".NAME NAM\
+        WHERE TOTTAB.ID = PUB.PUBLICATION_ID AND PUB.PERSON_ID = PER.PERSON_ID AND PER.PERSON_ID = NAM.PERSON_ID
+        GROUP BY TOTTAB.RN, PER.PERSON_ID`
     },
     notImp: {
         qPubTitleByKeyword: [
--- handlers/searchHandler.js
+++ handlers/searchHandler.js
@@ -2,6 +2,7 @@ const async = require('async');
 const connectionFactory = require('../db/connection');
 // const searchDB = require('../db/searchDB');
 const pattern = require('../db/searchQuery');
+const _ = require('lodash');
 
 const dummyConnect = function(cb) {
     let conn = {
@@ -12,103 +13,194 @@ const dummyConnect = function(cb) {
     cb(null, conn);
 };
 
-const doQuery = function(body, conn, cb) {
-    let queryBuilder;
-    try {
-        queryBuilder = pattern.searchType[body.type].begin;
-    } catch (err) {
-        if (err instanceof TypeError) {
-            cb(new Error('No search type specified.'));
-        } else {
-            throw err;
-        }
-        return;
-    }
-    if (body.params['person']) {
-        queryBuilder += pattern.nameTab.append;
-    }
-    queryBuilder += pattern.whereClause;
-    let pubTypeCount = 0;
-    let pubTypePredicate = pattern.pubType.begin;
-    for (const pubType of body.pubtype) {
-        if (pattern.pubType[pubType]) {
-            pubTypeCount++;
-            pubTypePredicate += pattern.pubType[pubType];
-        }
-    }
-    pubTypePredicate += pattern.pubType.end;
-    if (pubTypeCount != 0) {
-        queryBuilder += pubTypePredicate;
-    }
-    if (body.params['yearbegin']) {
-        queryBuilder += pattern.year.begin;
-    }
-    if (body.params['yearend']) {
-        queryBuilder += pattern.year.end;
-    }
-    if (body.params['person']) {
-        queryBuilder += pattern.nameTab.person;
-    }
-    if (body.params['title']) {
-        queryBuilder += pattern.title;
-    }
-    if (body['order']) {
-        if (pattern.order.type[body['order']['type']]) {
-            queryBuilder += pattern.order.type[body['order']['type']];
-            if (pattern.order.order[body['order']['order']]) {
-                queryBuilder += pattern.order.order[body['order']['order']];
+const handles = {
+    connectDB: connectionFactory.doconnect,
+
+    getPubType: [
+        'getBody',
+        function(body, cb) {
+            let pubTypeList = _.uniq(
+                _.map(_.filter(body.pubtype, x => pattern.pubType[x]), x => x.toUpperCase())
+            );
+            if (pubTypeList.length == 0) {
+                pubTypeList = pattern.allPubType;
             }
+            cb(null, pubTypeList);
         }
-    }
-    queryBuilder += pattern.searchType[body.type].end;
-    console.log(queryBuilder);
-    console.log(body.params);
-    conn.execute(queryBuilder, body.params, cb);
-};
+    ],
 
-// const refine = {
-//     PERSON_ID: function(row) {
-//         row["PERSON_ID"]
-//     }
-// };
-
-/**
- *
- */
-const reQuery = function(type, results, conn, cb) {
-    console.log(query.refine[type]);
-    async.map(results, refine, (err, results) => {
-        if (err) {
-            console.error(err.message);
+    buildQuery: [
+        'getBody',
+        'getPubType',
+        function(body, pubTypeList, cb) {
+            let queryBuilder;
+            try {
+                queryBuilder = pattern.searchType[body.type].begin;
+            } catch (err) {
+                if (err instanceof TypeError) {
+                    cb(new Error('No search type specified.'));
+                } else {
+                    throw err;
+                }
+                return;
+            }
+            if (body.params['person']) {
+                queryBuilder += pattern.nameTab.append;
+            }
+            if (body.params['journal']) {
+                queryBuilder += pattern.articleTab.append;
+            }
+            if (body.params['proceedingid']) {
+                queryBuilder += pattern.inproceedingTab.append;
+            }
+            if (body.params['incollectionid']) {
+                queryBuilder += pattern.incollectionTab.append;
+            }
+            queryBuilder += pattern.whereClause;
+            if (pubTypeList.length < 5) {
+                queryBuilder += pattern.pubType.begin;
+                queryBuilder.concat(..._.map(pubTypeList, x => pattern.pubType[x]));
+                queryBuilder += pattern.pubType.end;
+            }
+            if (body.params['person']) {
+                queryBuilder += pattern.nameTab.person;
+            }
+            if (body.params['journal']) {
+                queryBuilder += pattern.articleTab.article;
+            }
+            if (body.params['proceedingid']) {
+                queryBuilder += pattern.inproceedingTab.inproceeding;
+            }
+            if (body.params['incollectionid']) {
+                queryBuilder += pattern.incollectionTab.incollection;
+            }
+            if (body.params['yearbegin']) {
+                queryBuilder += pattern.year.begin;
+            }
+            if (body.params['yearend']) {
+                queryBuilder += pattern.year.end;
+            }
+            if (body.params['title']) {
+                queryBuilder += pattern.title;
+            }
+            if (body['order']) {
+                if (pattern.order.type[body['order']['type']]) {
+                    queryBuilder += pattern.order.type[body['order']['type']];
+                    if (pattern.order.order[body['order']['order']]) {
+                        queryBuilder += pattern.order.order[body['order']['order']];
+                    }
+                }
+            }
+            queryBuilder += pattern.searchType[body.type].end;
+            cb(null, queryBuilder, body.params);
         }
-        connectionFactory.dorelease(conn);
-        cb(results);
-    });
+    ],
+
+    findPerson: [
+        'buildQuery',
+        'connectDB',
+        function([queryBuilder, params], conn, cb) {
+            console.log(queryBuilder + pattern.refine.author);
+            conn.execute(queryBuilder + pattern.refine.author, params, (err, perResult) => {
+                if (err) {
+                    cb(err);
+                } else {
+                    cb(null, perResult['rows']);
+                }
+            });
+        }
+    ],
+
+    refinePerson: [
+        'findPerson',
+        function(people, cb) {
+            async.groupBy(
+                people,
+                (x, c) => c(null, x['RN']),
+                (error, result) =>
+                    async.mapValues(
+                        result,
+                        (v, k, callback) => async.map(v, (x, c) => c(null, x['AUTHOR']), callback),
+                        cb
+                    )
+            );
+        }
+    ],
+
+    doQuery: [
+        'buildQuery',
+        'getPubType',
+        'connectDB',
+        function([queryBuilder, params], pubTypeList, conn, cb) {
+            const refine = function(pubType, callback) {
+                console.log(
+                    queryBuilder +
+                        pattern.refine.pubbegin +
+                        pubType.toUpperCase() +
+                        pattern.refine.pubend
+                );
+                conn.execute(
+                    queryBuilder +
+                        pattern.refine.pubbegin +
+                        pubType.toUpperCase() +
+                        pattern.refine.pubend,
+                    params,
+                    (err, result) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(null, result['rows']);
+                        }
+                    }
+                );
+            };
+            async.map(pubTypeList, refine, (err, results) => {
+                cb(err, _.flatten(results));
+            });
+        }
+    ],
+
+    combineResult: [
+        'refinePerson',
+        'doQuery',
+        function(people, pubs, cb) {
+            async.map(
+                pubs,
+                (x, c) => {
+                    x['AUTHOR'] = people[x['RN']];
+                    c(null, x);
+                },
+                cb
+            );
+        }
+    ],
+
+    disconnectDB: [
+        'combineResult',
+        'connectDB',
+        function(result, conn, cb) {
+            connectionFactory.dorelease(conn);
+            cb(null, result);
+        }
+    ]
 };
 
 const searchHandler = function(req, res) {
     // if (!req.user) res.sendStatus(401);
-    async.waterfall(
-        [
-            connectionFactory.doconnect,
-            // dummyConnect,
-            async.apply(doQuery, req.body)
-            // reQuery
-        ],
-        (err, qResult) => {
-            if (err) {
-                console.log(err);
-                res.status(400).json({
-                    success: false
-                });
-            } else {
-                res.status(200).json({
-                    success: true,
-                    result: qResult
-                });
-            }
+    handles.getBody = cb => cb(null, req.body);
+    async.autoInject(handles, (err, qResult) => {
+        if (err) {
+            console.log(err);
+            res.status(400).json({
+                success: false
+            });
+        } else {
+            res.status(200).json({
+                success: true,
+                result: qResult.disconnectDB
+            });
         }
-    );
+    });
 };
 
 module.exports = searchHandler;
